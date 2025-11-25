114fe67abcc38bfe7e548c52452b1283682212dd
Bryan Alejandro Alamillo Betancourt <31224002+Dark110@users.noreply.github.com>
2025-11-25 15:48:18 -0600

    Script Menu

--- Assets/SpritesUI/BackgroundMovement.cs
+++ Assets/SpritesUI/BackgroundMovement.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
-public class BackgroundMovement : MonoBehaviour
+public class BackgroundBreathing : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Configuración del Zoom")]
+    [Tooltip("La velocidad a la que respira el fondo")]
+    public float speed = 1.0f;
+
+    [Tooltip("Qué tanto se va a agrandar (0.05 es un 5% más grande)")]
+    public float zoomStrength = 0.05f;
+
+    private Vector3 initialScale;
+
     void Start()
     {
-        
+        initialScale = transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float scaleOffset = Mathf.Sin(Time.time * speed) * zoomStrength;
+
+        // Aplicamos la nueva escala (x, y, z)
+        transform.localScale = initialScale + (Vector3.one * scaleOffset);
     }
 }
